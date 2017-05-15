a5a7e43f2a4bfee2c222ef255f9e6b8bfdbdf329
mat cort <mat.cort@gmail.com>
2017-05-15 16:41:36 +0000

    12052017

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,18 +6,18 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-CursoDictadoUsuario.delete_all
-Usuario.create(nombre:"Mati", rut:1)
-CursoDictadoUsuario.create(usuario_id:395, curso_dictado_id:9, aprobado:"1")
-CursoDictado.delete_all
-Estudio.delete_all
-HorarioCurso.delete_all
-Usuario.delete_all
-Curso.delete_all
+#CursoDictadoUsuario.delete_all
+#Usuario.create(nombre:"Mati", rut:1)
+#CursoDictadoUsuario.create(usuario_id:395, curso_dictado_id:9, aprobado:"1")
+#CursoDictado.delete_all
+#Estudio.delete_all
+#HorarioCurso.delete_all
+#Usuario.delete_all
+#Curso.delete_all
 
-Curso.create(nombre:"Curso A")
-Curso.create(nombre:"Curso B")
-Curso.create(nombre:"Curso C")
+#Curso.create(nombre:"Curso A")
+#Curso.create(nombre:"Curso B")
+#Curso.create(nombre:"Curso C")
 
-Usuario.create(nombre:"Admin", rut:1, password:"123", password_confirmation:"123", rol:"1", mail:"a")
+Usuario.create(nombre:"Admin", rut:1, password:"12345678", password_confirmation:"12345678", rol:"1", mail:"a")
 
